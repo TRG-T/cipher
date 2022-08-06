ad01a91028166af2309e5cc252a75fc79bfd04b0
TRG-T <lockontarget@protonmail.com>
2022-08-06 20:34:06 +0200

    c: revstr function added

--- c/main.c
+++ c/main.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
+#include <string.h>
 
-const char *encrypt(char text[255], short int key) {
-    for (char i = 0; text[i] != 10; i++) {
+const char* encrypt(char text[255], short int key) {
+    for (char i = 0; i < strlen(text)-1; i++) {
         text[i] += key;
     }
     return text;
 }
 
+void revstr(char text[255], char reversed_string[255]) {
+    for (char i = 0; i < strlen(text)-1; i++) {
+        reversed_string[i] = text[strlen(text)-1-i];
+    }
+}
+
 int main() {
     char user_text[255];
     char user_key[255];
+    char reversed_string[255];
     const char LATIN[5][6] = {
         {'a', 'b', 'c', 'd', 'e', 'f'},
         {'g', 'h', 'i', 'j', 'k', 'l'},
@@ -18,7 +26,8 @@ int main() {
         {'y', 'z', '!', '?', ':', ' '},
     };
 
-    const char* GALACTIC[5][6] = {
+    // third index is 4 because it stores utf8 chars
+    const char GALACTIC[5][6][4] = {
         {"ᔑ", "ʖ", "ᓵ", "↸", "Ŀ", "⎓"},
         {"ㅓ", "〒", "╎", "፧", "ꖌ", "ꖎ"},
         {"ᒲ", "リ", "フ", "¡", "ᑑ", "።"},
@@ -28,9 +37,11 @@ int main() {
 
     printf("Enter text to be encrypted\n");
     fgets(user_text, 255, stdin);
+    revstr(user_text, reversed_string);
+    printf("%s\n", reversed_string);
 
     printf("Enter key (number or string)\n");
     fgets(user_key, 255, stdin);
 
-    printf("%s", encrypt(user_text, 3));
+    printf("%s", encrypt(reversed_string, 3));
 }
