c14f1c2e9c6d46754fad4209819bd3d93039b22c
TRG-T <lockontarget@protonmail.com>
2022-08-08 02:21:12 +0200

    feat: add index_of and encrypt function

--- c/main.c
+++ c/main.c
@@ -9,7 +9,7 @@ const char LATIN[5][6] = {
     {'y', 'z', '!', '?', ':', ' '},
 };
 
-// third index is 4 because it stores utf8 chars
+// third index is 4 because utf-8 string can store up to 4 bytes
 const char GALACTIC[5][6][4] = {
     {"ᔑ", "ʖ", "ᓵ", "↸", "Ŀ", "⎓"},
     {"ㅓ", "〒", "╎", "፧", "ꖌ", "ꖎ"},
@@ -18,17 +18,32 @@ const char GALACTIC[5][6][4] = {
     {"॥", "Λ", "ʗ", "˨", "ᚴ", "ᚌ"},
 };
 
-const char* encrypt(char text[255], unsigned short key) {
-    for (char i = 0; i < strlen(text)-1; i++) {
-        text[i] += key;
+void index_of(char coords[2], char letter) {
+    for (char i = 0; i < 5; i++) {
+        for (char j = 0; j < 6; j++) {
+            if (letter == LATIN[i][j]) {
+                coords[0] = i;
+                coords[1] = j;
+            }
+        }
     }
-    return text;
+}
+
+void encrypt(char text[255], unsigned short key, char final_text[255][4]) {
+    char coords[2];
+    for (char i = 0; i < strlen(text)-1; i++) {
+        index_of(coords, text[i]);
+        for (char j = 0; j < 4; j++) {
+            final_text[i][j] = GALACTIC[coords[0]][(coords[1] + key) % 6][j];
+        }
+    }
 }
 
 void revstr(char text[255], char reversed_string[255]) {
     for (char i = 0; i < strlen(text)-1; i++) {
-        reversed_string[i] = text[strlen(text)-2-i];
+        reversed_string[i] = text[strlen(text) - 2 - i];
     }
+    reversed_string[strlen(reversed_string) - 1] = '\0';
 }
 
 unsigned short user_key_to_key(char user_key[]) {
@@ -43,6 +58,7 @@ int main() {
     char user_text[255];
     char user_key[255];
     char reversed_string[255];
+    char final_text[255][4];
     unsigned short key;
 
     printf("Enter text to be encrypted\n");
@@ -56,5 +72,8 @@ int main() {
     key = user_key_to_key(user_key);
     printf("key - %hu\n", key);
 
-    printf("%s", encrypt(reversed_string, key));
+    encrypt(reversed_string, key, final_text);
+    for (char i = 0; i < strlen(reversed_string)-1; i++) {
+        printf("%s", final_text[i]);
+    }
 }
