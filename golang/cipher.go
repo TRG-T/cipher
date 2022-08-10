eb4436350cdaa8ba4e6130ba7b4ca4c9dc198f53
TRG-T <lockontarget@protonmail.com>
2022-08-10 02:15:55 +0200

    add older langs

---
+++ golang/cipher.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"github.com/manifoldco/promptui"
+)
+
+var latin = [5][6]string{
+	{"a", "b", "c", "d", "e", "f"},
+	{"g", "h", "i", "j", "k", "l"},
+	{"m", "n", "o", "p", "q", "r"},
+	{"s", "t", "u", "v", "w", "x"},
+	{"y", "z", "!", "?", ":", " "},
+}
+
+var galactic = [5][6]string{	
+	{"ᔑ", "ʖ", "ᓵ", "↸", "Ŀ", "⎓"},
+	{"ㅓ", "〒", "╎", "፧", "ꖌ", "ꖎ"},
+	{"ᒲ", "リ", "フ", "¡", "ᑑ", "።"},
+	{"ነ", "ﬧ", "⚍", "⍊", "∴", "/"},
+	{"॥", "Λ", "ʗ", "˨", "ᚴ", "ᚌ"},
+}
+var userText string
+var userKey string
+
+func indexOf(letter string, choice int) (int, int) {
+	for i:=0; i<5; i++ {
+		for a:=0; a<6; a++ {
+			if choice == 0 {
+				if latin[i][a] == letter {
+					return i, a;
+				}
+			} else {
+				if galactic[i][a] == letter {
+					return i, a;
+				}
+			}
+		}
+	}
+	return 0, 0;
+}
+
+func reverse(text []string) []string {
+	for i, a:= 0, len(text)-1; i<a; i, a = i+1, a-1 {
+		text[i], text[a] = text[a], text[i];
+	}
+	return text;
+}
+
+func stringToKey(key []string) int {
+	var keyInt int
+	for i:=0; i<len(key); i++ {
+		textRow, textColumn := indexOf(key[i], 0)
+		keyInt += textRow + textColumn
+	}
+	fmt.Println(keyInt)
+	return keyInt
+}
+
+func encrypt(text []string, key int) string {
+	for i:=0; i<len(text); i++ {
+		row, col := indexOf(text[i], 0);
+		text[i] = galactic[row][(col+key)%6]
+    }
+    return strings.Join(text, "");
+}
+
+func decrypt(text []string, key int) string {
+	for i:=0; i<len(text); i++ {
+		row, col := indexOf(text[i], 1);
+		text[i] = latin[row][(col+(key-2))%6]
+    }
+    return strings.Join(text, "");
+}
+
+func main() {
+	prompt := promptui.Select{
+		Label: "What to do",
+		Items: []string{"Encrypt", "Decrypt"},
+	}
+	_, result, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed: %v\n", err)
+		return
+	}
+
+	fmt.Println("Enter the text")
+	fmt.Scanln(&userText)
+	fmt.Println("Enter key (only text): ")
+	fmt.Scanln(&userKey)
+	key := stringToKey(strings.Split(userKey, ""));
+	text := strings.Split(userText, "");
+	fmt.Printf("Original text: %v \n", text);
+	reversedText := reverse(text);
+	fmt.Printf("Reversed text: %v \n", reversedText);
+	if result == "Encrypt" {
+		fmt.Printf("Encrypted text: %v \n", encrypt(reversedText, key));
+	} else {
+		fmt.Printf("Decrypted text: %v \n", decrypt(reversedText, key));
+	}
+}
---
+++ rs/src/main.rs
@@ -0,0 +1,106 @@
+use console::Term;
+use dialoguer::{theme::ColorfulTheme, Select};
+use std::io;
+
+const LATIN: [[char; 6]; 5] = [
+    ['a', 'b', 'c', 'd', 'e', 'f'],
+    ['g', 'h', 'i', 'j', 'k', 'l'],
+    ['m', 'n', 'o', 'p', 'q', 'r'],
+    ['s', 't', 'u', 'v', 'w', 'x'],
+    ['y', 'z', '!', '?', ':', ' '],
+];
+
+const GALACTIC: [[char; 6]; 5] = [
+    ['ᔑ', 'ʖ', 'ᓵ', '↸', 'Ŀ', '⎓'],
+    ['ㅓ', '〒', '╎', '፧', 'ꖌ', 'ꖎ'],
+    ['ᒲ', 'リ', 'フ', '¡', 'ᑑ', '።'],
+    ['ነ', 'ﬧ', '⚍', '⍊', '∴', '/'],
+    ['॥', 'Λ', 'ʗ', '˨', 'ᚴ', 'ᚌ'],
+];
+
+fn main() -> std::io::Result<()> {
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .item("Encrypt")
+        .item("Decrypt")
+        .default(0)
+        .interact_on(&Term::stderr())?;
+
+    println!("Enter the text");
+    let mut user_text = String::new();
+    io::stdin()
+        .read_line(&mut user_text)
+        .expect("Failed to read input");
+    let text = user_text.trim();
+
+    println!("Enter the key (text or number)");
+    let mut user_key = String::new();
+    io::stdin()
+        .read_line(&mut user_key)
+        .expect("Failed to read input");
+    let key = string_to_key(user_key.trim());
+
+    match selection {
+        0 => {
+            let encrypted_text = encrypt(text, key);
+            println!("Encrypted text: {}", encrypted_text);
+        }
+        1 => {
+            let decrypted_text = decrypt(text, key);
+            println!("Decrypted text: {}", decrypted_text);
+        }
+        _ => {
+            println!("Something went wrong");
+            println!("Something went wrong")
+        }
+    }
+    Ok(())
+}
+
+fn index_of(letter: char, choice: usize) -> (usize, usize) {
+    [LATIN, GALACTIC]
+        .get(choice)
+        .and_then(|map| {
+            map.iter().enumerate().find_map(|(row, row_slice)| {
+                let col = row_slice.iter().position(|&s| s == letter)?;
+                Some((row, col))
+            })
+        })
+        .unwrap_or((0, 0))
+}
+
+fn string_to_key(text: &str) -> usize {
+    let mut key = 0;
+    for c in text.chars() {
+        match c.is_alphabetic() {
+            true => {
+                let (row, col) = index_of(c, 0);
+                key += row + col;
+            }
+            false => {
+                key += c.to_digit(10).unwrap_or(0) as usize
+            }
+        }
+    }
+    println!("Key is: {}", key);
+    key
+}
+
+fn encrypt(text: &str, key: usize) -> String {
+    text.chars()
+        .rev()
+        .map(|letter| {
+            let (row, col) = index_of(letter, 0);
+            GALACTIC[row][(col + key) % 6]
+        })
+        .collect()
+}
+
+fn decrypt(text: &str, key: usize) -> String {
+    text.chars()
+        .rev()
+        .map(|letter| {
+            let (row, col) = index_of(letter, 1);
+            LATIN[row][(col + (key-2)) % 6]
+        })
+        .collect()
+}
